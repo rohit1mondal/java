8d4cf31f43cdae43524968a78307f9b7fa3fb0a7
Rohit Mondal <rohitmondal13112001@gmail.com>
2024-11-11 16:33:51 +0530

    arrays

--- apna college/video 10/array2.java
+++ apna college/video 10/array2.java
@@ -7,12 +7,19 @@ public class array2 {
         int size = sc.nextInt();
         int numbers[] = new int[size];
 
+        // input
         for (int i = 0; i < size; i++) {
             numbers[i] = sc.nextInt();
         }
 
+        int x = sc.nextInt();
+
+        // output
         for (int i = 0; i < size; i++) {
-            System.out.println(numbers[i]);
+            // System.out.println(numbers[i]);
+            if (numbers[i] == x) {
+                System.out.println("x found at index : " + i);
+            }
         }
     }
 }
