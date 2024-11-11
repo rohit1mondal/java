37fddb4ad4511cee9245ca4a2990de3c6a6b1e76
Rohit Mondal <rohitmondal13112001@gmail.com>
2024-11-11 18:15:14 +0530

    patterns

--- apna college/video 5/patterns.java
+++ apna college/video 5/patterns.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class patterns {
     public static void main(String[] args) {
         // print the patterns
@@ -43,17 +45,16 @@ public class patterns {
 
         // 5th pattern (Inverted & rotated-180 half pyramid)
         // for (int i = 1; i <= n; i++) {
-        //     // inner loop ->spaces
-        //     for (int j = 1; j <= n - i; j++) {
-        //         System.out.print(" ");
-        //     }
-        //     // inner loop -> stars
-        //     for (int j = 1; j <= i; j++) {
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
+        // // inner loop ->spaces
+        // for (int j = 1; j <= n - i; j++) {
+        // System.out.print(" ");
+        // }
+        // // inner loop -> stars
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
         // }
 
-        
     }
 }
