8a116d90390e8c4e7b60464343ce69367d28b30d
Rohit Mondal <rohitmondal13112001@gmail.com>
2024-11-11 16:02:24 +0530

    patterns

---
+++ apna college/video 5/patterns.java
@@ -0,0 +1,59 @@
+public class patterns {
+    public static void main(String[] args) {
+        // print the patterns
+        int n = 4;
+        // int m = 5;
+
+        // 1st pattern (solid rectangle)
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= m; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // 2nd pattern (hollow rectangle)
+        // outer loop
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= m; j++) {
+        // if(i==1 || j==1 || i==n || j==m){
+        // System.out.print("*");
+        // }else{
+        // System.out.print(" ");
+        // }
+        // }
+        // System.out.println();
+        // }
+
+        // 3rd pattern (Half pyramid)
+        // for (int i =1;i<=n;i++){
+        // for(int j = 1;j<=i;j++){
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // 4th pattern(Inveerted half pyramid)
+        // for (int i = n;i>=1;i--){
+        // for (int j = 1;j<=i;j++){
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // 5th pattern (Inverted & rotated-180 half pyramid)
+        // for (int i = 1; i <= n; i++) {
+        //     // inner loop ->spaces
+        //     for (int j = 1; j <= n - i; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     // inner loop -> stars
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        
+    }
+}
---
+++ apna college/video 5/patterns2.java
@@ -0,0 +1,54 @@
+public class patterns2 {
+
+    public static void main(String[] args) {
+        // 6th pattern (Pyramid with Numbers)
+
+        // int n = 5;
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print(j + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // 7th pattern (Inverted half pyramid with numbers)
+        // int n = 5;
+
+        // //outer loop
+        // for (int i =1; i<=n;i++){
+        //     //inner loop
+        //     for (int j=1; j<=n-i+1; j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+        //8th pattern (Floyd's triangle)
+        // int n =5;
+        // int number = 1;
+
+        // for (int i = 1; i<=n; i++){
+        //     for (int j = 1; j<=i; j++){
+        //         System.out.print(number+ " ");
+        //         number++;
+        //     }
+        //     System.out.println();
+        // }
+
+        // 9th pattern (0-1 triangle)
+
+        int n = 5;
+
+        for (int i = 1;i <= n;i++){
+            for(int j=1;j<=i;j++){
+                int sum = i+j;
+                if(sum % 2 == 0){
+                    System.out.print("1 ");
+                } else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
