15205585656350f1d31fec502cdeefaa65e5634e
Rohit Mondal <rohitmondal13112001@gmail.com>
2024-11-11 16:09:29 +0530

    arrays

---
+++ apna college/video 10/array.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class array {
+
+    public static void main(String[] args) {
+        // int[] marks = new int[3];
+        int marks[] = new int[3];
+        marks[0] = 88;
+        marks[1] = 90;
+        marks[2] = 96;
+            
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
