7fd65a81099ffccce99edea1b2d5e5bb6276b1fb
Rohit Mondal <rohitmondal13112001@gmail.com>
2024-11-11 15:59:50 +0530

    loops

---
+++ apna college/video 4/exercise.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class exercise {
+    // public static void main(String[] args) {
+    //     print the sum of First n natural numbers!
+    //     Scanner sc = new Scanner(System.in);
+    //     int n = sc.nextInt();
+
+    //     int sum = 0;
+    //     for (int i = 1; i <= n; i++) {
+    //         sum = sum + i;
+    //     }
+    //     System.out.println(sum);
+    // }
+
+    public static void main(String[] args) {
+        // print the table of a number input by the user!
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i =1;i<11;i++){
+            System.out.println(i*n);
+        }
+    }
+}
---
+++ apna college/video 4/loops.java
@@ -0,0 +1,19 @@
+public class loops {
+    public static void main(String[] args) {
+        // for(int i = 0;i<11;i++ ){
+        // System.out.println("hey!");
+        // }
+
+        // int i = 0;
+        // while (i < 11) {
+        //     System.out.println("hi!");
+        //     i = i + 1;
+        // }
+
+        // int i = 0;
+        // do {
+        //     System.out.println("hello!");
+        //     i++;
+        // } while (i<11);
+    }
+}
